c2294598420506d88652c4bcfbaa886cd1b79d53
mi-araf <arafmushfiq@gmail.com>
2022-07-27 17:41:36 +0600

    more about map, find, filter with arr.-obj.

---
+++ object-property.js
@@ -0,0 +1,31 @@
+const students = [
+    {id: 17, nam: "Sabrina Carpenter"},
+    {id: 26, nam: "Ross Lynch"},
+    {id: 29, nam: "Lauro Marano"},
+    {id: 7 , nam: "Mushfiq Araf"},
+]
+
+// 1) map
+const names = students.map( s => s.nam);
+const ids = students.map( s => s.id);
+console.log(names);
+console.log(ids);
+
+// 2) filter = find/filter all the element;
+const idFilter = students.filter( s => s.id > 20);
+console.log(idFilter);
+
+// 3) find = find/filter the first element
+const idFind = students.find( s => s.id > 20);
+console.log(idFind);
+
+// 4) get the nam: (tradational way)
+let newArr = [];
+for (let i = 0; i < students.length; i++) {
+    let element = students[i];
+    let eleName = element.nam;
+    // console.log(eleName);
+    newArr.push(eleName);
+}
+console.log(newArr);
+console.log(...newArr);
